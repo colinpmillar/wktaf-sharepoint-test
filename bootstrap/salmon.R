0776da17ebc673830776595e605a3f2abd2af916
Colin Millar <colin.millar@ices.dk>
2021-02-02 12:19:42 +0100

    add link to ggplot2 data documentation using roxygen

--- bootstrap/salmon.R
+++ bootstrap/salmon.R
@@ -8,6 +8,9 @@
 #' @tafSource script
 #' @references \url{https://data.marine.gov.scot/dataset/girnock-and-baddoch-emigrant-numbers-year-emigration}
 
+# some examples of other feilds you might want to add if you are really keen:
+# https://github.com/tidyverse/ggplot2/blob/master/R/data.R
+
 library(icesTAF)
 
 salmon <- read.csv("https://data.marine.gov.scot/sites/default/files//Girnock_Baddoch_Emigrant_Numbers_by_Year_of_Emigration_2020.csv")
